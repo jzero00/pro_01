5621fa6720b91ad7ae4ec581a4db5b6a28eb5f32
jzero00 <57884057+jzero00@users.noreply.github.com>
2020-11-17 23:20:52 +0900

    board table create query

---
+++ board_table.sql
@@ -0,0 +1,10 @@
+-- board table Ãß°¡
+
+CREATE TABLE board (
+	"type" varchar NOT NULL,
+	"no" varchar NOT NULL,
+	title varchar NOT NULL,
+	"content" text NULL,
+	hit int4 NULL,
+	reg_date date NULL
+);
